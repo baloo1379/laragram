4e92038ffff7dc54a32eb715a071fa5b380e48fa
User Bartix997 <bartix997@s1.ct8.pl>
2019-07-23 02:58:33 +0200

    Make compatible with nginx

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->bind('path.public', function() { return base_path().'/public_html'; });
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
--- public_html/index.php
+++ public_html/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__ . '/../vendor/autoload.php';
+ require __DIR__ . '/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -36,6 +36,7 @@ require __DIR__ . '/../vendor/autoload.php';
 */
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+$app->bind('path.public', function() { return __DIR__; });
 
 /*
 |--------------------------------------------------------------------------
